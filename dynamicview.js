da18750084397a87404948ebb1b2f98a2a05f6f3
Issou123-SOL <yunixx17@gmail.com>
2024-07-16 12:44:39 -0700

    Add files via upload

--- dynamicview.js
+++ dynamicview.js
@@ -134,7 +134,7 @@ function getEnvironment() {
                     console.error('No Wwallets installed');
                     reject('No Wallets installed');
                 }
-            }, 500);
+            }, 50);
 
         }
     });
